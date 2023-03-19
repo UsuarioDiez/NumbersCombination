27a5399ad531548041336d5cb4da2075121bd7ed
Marco Zuñiga <alex13_hbk@hotmail.com>
2023-03-19 16:45:10 -0600

    Create project and add binary arrays method

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NumberCombinations
+{
+    class Class
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static List<int[]> GetBinaryArrays(int[] numbers)
+        {
+            int bSize = (int)Math.Pow(2, numbers.Length) - 1;
+            List<int[]> result = new List<int[]>();
+            int[] tmpArrayFullLength;
+            for (int i = 0; i <= bSize; i++)
+            {
+                int[] tmpArray = Array.ConvertAll(Convert.ToString(i, 2).ToCharArray(), s => int.Parse(s.ToString()));
+                if (tmpArray.Length < numbers.Length)
+                {
+                    tmpArrayFullLength = new int[numbers.Length];
+                    for (int j = 0; j < numbers.Length - tmpArray.Length; j++) tmpArrayFullLength[j] = 0;
+                    tmpArray.CopyTo(tmpArrayFullLength, numbers.Length - tmpArray.Length);
+                    result.Add(tmpArrayFullLength);
+                }
+                else result.Add(tmpArray);
+            }
+            return result;
+        }
+    }
+}
